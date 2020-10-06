3bcf47eaa9c772f8c04341f02e74b0d6eb55c40e
Christian Villere <christianvillere@gmail.com>
2020-10-05 20:06:12 -0700

    Finish bubble sort problem

--- bubble.rb
+++ bubble.rb
@@ -1,2 +1,15 @@
-def bubble_sort
+def bubble_sort unsorted_array
+  (unsorted_array.length - 1).times do
+    unsorted_array.each_with_index do |item, index|
+      if (index != unsorted_array.length - 1) && (item > unsorted_array[index + 1])
+      unsorted_array[index] = unsorted_array[index + 1]
+      unsorted_array[index + 1] = item
+      end
+    end
+  end
+
+ unsorted_array
+
 end
+
+p bubble_sort([4,3,78,2,0,2,-10,67,45])
